91e2fbc96c3dfcf89da486174ed1780402b2da27
aliyaskcodes <aliyashaikh0709@eng.rizvi.edu.in>
2026-04-01 08:58:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # AIM: Create a personalized greeting message
 #      User enters name: eg: Shibu. and the program should print "Hello, Shibu" 
-# Coder: 
-# Date:
+# Coder: shaikh aliya
+# Date: 1-4-26
 
 # Write your code here
+name = input("Enter your name: ")
+print("Good afternoon", name)
+print("Rizvi College of Engineering")
